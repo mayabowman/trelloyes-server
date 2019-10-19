91891bb7e2b02c69622f31cd9c4cd8b3a17f5a6e
Maya Bowman <mayashady1@gmail.com>
2019-10-19 11:46:19 -0400

    fixed error in logger require

--- src/card/card-router.js
+++ src/card/card-router.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const uuid = require('uuid/v4')
-const logger = require('/..logger')
+const logger = require('../logger')
 const { cards, lists } = require('../store')
 
 const cardRouter = express.Router()
